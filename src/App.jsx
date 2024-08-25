fd047705f7b9fa7196e8011afab2c1535d94e94b
Shaun Oosthuizen <shaun.oosthuizen.wp@gmail.com>
2024-08-25 14:08:15 +0200

    Made some restructuring changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
 import Content from "./components/Content";
-import Sidebar from "./components/sidebar";
-import { useState } from "react";
 
 function App() {
-  const [projects, setProjects] = useState([]);
-  const [isAddingNewProject, setIsAddingNewProject] = useState(false);
-  const [selectedProject, setSelectedProject] = useState(null);
-
-  function handleCreateNewProject() {
-    setIsAddingNewProject(true);
-    setSelectedProject(null)
-  }
-
-  function hideNewProjectForm() {
-    setIsAddingNewProject(false);
-  }
-
-  function addProject(newProject) {
-    setProjects((prevProjects) => {
-      const newProjectsList = [...prevProjects];
-      newProject.id = newProjectsList.length + 1;
-      newProjectsList.push(newProject);
-
-      return newProjectsList;
-    });
-  }
-
-  function handleSelectProject(project) {
-    if (project.id === selectedProject?.id) {
-      setSelectedProject(null);
-    } else {
-      setSelectedProject(project);
-    }
-  }
-
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar onCreateNewProject={handleCreateNewProject} onProjectSelect={handleSelectProject} projects={projects} selectedProject={selectedProject} ></Sidebar>
-      <Content onCreateNewProject={handleCreateNewProject} addProject={addProject} hideNewProjectForm={hideNewProjectForm} isAddingNewProject={isAddingNewProject} selectedProject={selectedProject}></Content>
+      <Content/>
     </main>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,59 @@
 import NewProject from "./NewProject";
-import { useState } from "react";;
+import ViewProject from "./ViewProject";
+import Sidebar from "./Sidebar";
 
-export default function Content({onCreateNewProject, addProject, hideNewProjectForm, isAddingNewProject}) {
+import { useState, useRef } from "react";;
 
- 
+export default function Content() {
+  const [projects, setProjects] = useState([]);
+  const [isAddingNewProject, setIsAddingNewProject] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  function handleCreateNewProject() {
+    setIsAddingNewProject(true);
+    setSelectedProject(null)
+  }
+
+  function hideNewProjectForm() {
+    setIsAddingNewProject(false);
+  }
+
+  function addProject(newProject) {
+    setProjects((prevProjects) => {
+      const newProjectsList = [...prevProjects];
+      newProject.id = newProjectsList.length + 1;
+      newProjectsList.push(newProject);
+
+      return newProjectsList;
+    });
+  }
+
+  function handleSelectProject(project) {
+    if (project.id === selectedProject?.id) {
+      setSelectedProject(null);
+    } else {
+      setSelectedProject(project);
+    }
+  }
+
 
   return (
     <>
+      <Sidebar onCreateNewProject={handleCreateNewProject} onProjectSelect={handleSelectProject} projects={projects} selectedProject={selectedProject} />
+
+      {(!isAddingNewProject && !selectedProject) &&
+        <div className="mt-24 text-center w-2/3">
+          <img src="src\assets\no-projects.png" className="w-16 h-16 object-contain mx-auto" />
+          <h2 className="text-xl font-bold text-stone-700 my-4">No Project Selected</h2>
+          <p className="text-stone-600 mb-4">Select a project or get started with a new one</p>
+          <button onClick={handleCreateNewProject} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Create new project</button>
+        </div>
+      }
       {isAddingNewProject && <NewProject handleAddProject={addProject} hideNewProjectForm={hideNewProjectForm} />}
-      {isAddingNewProject || <div className="mt-24 text-center w-2/3">
-        <img src="src\assets\no-projects.png" className="w-16 h-16 object-contain mx-auto" />
-        <h2 className="text-xl font-bold text-stone-700 my-4">No Project Selected</h2>
-        <p className="text-stone-600 mb-4">Select a project or get started with a new one</p>
-        <button onClick={onCreateNewProject} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Create new project</button>
-      </div>}
+
+      {(!isAddingNewProject && !!selectedProject) &&
+        <ViewProject project={selectedProject} />
+      }
     </>
 
   )
